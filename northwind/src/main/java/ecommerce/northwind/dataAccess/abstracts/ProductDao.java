e666b414cbc379ea6e280d7c7eb563599a85696b
Alperen Mutlu <chetto_17@hotmail.com>
2021-05-27 22:56:45 +0300

    CategoryId-->Category olarak düzeltildi.

--- northwind/src/main/java/ecommerce/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/ecommerce/northwind/dataAccess/abstracts/ProductDao.java
@@ -11,20 +11,20 @@ public interface ProductDao extends JpaRepository<Product,Integer> {
 	Product getByProductName(String productName);
 	
 	//select * from products where product_name = abc and category_id =1 gibi
-	Product getByProductNameAndCategoryId(String productName, int categoryId);
+	Product getByProductNameAndCategory(String productName, int categoryId);
 	
 	//select * from products where product_name = abc or category_id =1 gibi
-	List<Product> getByProductNameOrCategoryId(String productName, int categoryId);
+	List<Product> getByProductNameOrCategory(String productName, int categoryId);
 	
 	//select * from products where category_id in(1,2,3,4)  category id 1,2,3,4 olanları getirir
-	List<Product> getByCategoryIdIn(List<Integer> categories);
+	List<Product> getByCategoryIn(List<Integer> categories);
 	
 	
 	List<Product> getByProductNameContains(String productName);
 	
 	List<Product> getByProductNameStartsWith(String productName);
 	
-	@Query("From Product where productName=:productName and categoryId=:categoryId")
+	@Query("From Product where productName=:productName and category.categoryId=:categoryId")
 	List<Product> getByNameAndCategory(String productName, int categoryId);
 	
 }
