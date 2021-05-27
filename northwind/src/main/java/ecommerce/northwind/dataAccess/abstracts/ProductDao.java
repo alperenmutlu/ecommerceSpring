288a13343945be8efbb450a68f34b275da657c7a
Alperen Mutlu <chetto_17@hotmail.com>
2021-05-28 00:16:35 +0300

    Category içeren metotların sonuna _CategoryId eklendi.

--- northwind/src/main/java/ecommerce/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/ecommerce/northwind/dataAccess/abstracts/ProductDao.java
@@ -11,10 +11,10 @@ public interface ProductDao extends JpaRepository<Product,Integer> {
 	Product getByProductName(String productName);
 	
 	//select * from products where product_name = abc and category_id =1 gibi
-	Product getByProductNameAndCategory(String productName, int categoryId);
+	Product getByProductNameAndCategory_CategoryId(String productName, int categoryId);
 	
 	//select * from products where product_name = abc or category_id =1 gibi
-	List<Product> getByProductNameOrCategory(String productName, int categoryId);
+	List<Product> getByProductNameOrCategory_CategoryId(String productName, int categoryId);
 	
 	//select * from products where category_id in(1,2,3,4)  category id 1,2,3,4 olanları getirir
 	List<Product> getByCategoryIn(List<Integer> categories);
