2b8f56581aee766138e8d97d970a4083f69e07c7
Alperen Mutlu <chetto_17@hotmail.com>
2021-05-27 21:52:57 +0300

    Kendi metotlarımızı yazdık.

--- northwind/src/main/java/ecommerce/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/ecommerce/northwind/dataAccess/abstracts/ProductDao.java
@@ -1,9 +1,30 @@
 package ecommerce.northwind.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import ecommerce.northwind.entities.concretes.Product;
 
 public interface ProductDao extends JpaRepository<Product,Integer> {
+	Product getByProductName(String productName);
+	
+	//select * from products where product_name = abc and category_id =1 gibi
+	Product getByProductNameAndCategoryId(String productName, int categoryId);
+	
+	//select * from products where product_name = abc or category_id =1 gibi
+	List<Product> getByProductNameOrCategoryId(String productName, int categoryId);
+	
+	//select * from products where category_id in(1,2,3,4)  category id 1,2,3,4 olanları getirir
+	List<Product> getByCategoryIdIn(List<Integer> categories);
+	
+	
+	List<Product> getByProductNameContains(String productName);
+	
+	List<Product> getByProductNameStartsWith(String productName);
+	
+	@Query("From Product where productName=:productName and categoryId=:categoryId")
+	List<Product> getByNameAndCategory(String productName, int categoryId);
 	
 }
